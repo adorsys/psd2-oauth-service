dfd0382c956bb995f415057c6a35af93667c677b
Serhii Petrychenko <spe@adorsys.com.ua>
2019-10-18 10:58:52 +0300

    remove unnecessary annotation

--- oauth-rest-server/src/main/java/de/adorsys/xs2a/adapter/controller/TokenController.java
+++ oauth-rest-server/src/main/java/de/adorsys/xs2a/adapter/controller/TokenController.java
@@ -11,13 +11,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping
 public class TokenController {
 
     private static final Logger logger = LoggerFactory.getLogger(TokenController.class);
